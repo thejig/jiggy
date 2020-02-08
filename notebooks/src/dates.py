eb6af379b294f4d95bcb6540a512a597391c96d1
leonkozlowski <leonkozlowski@gmail.com>
2020-02-08 14:47:47 -0500

    new task

--- notebooks/src/dates.py
+++ notebooks/src/dates.py
@@ -15,3 +15,10 @@ class GetWeekdayTask(Task):
         super(GetWeekdayTask, self).__init__(name)
     def run(self, x):
         return x.strftime('%A')
+
+class GetFirstLetter(Task):
+    def __init__(self, name):
+        self.name = name
+        super(GetFirstLetter, self).__init__(name)
+    def run(self, x):
+        return x[0]
