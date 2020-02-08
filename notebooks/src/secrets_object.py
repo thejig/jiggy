bf42d84eded91390893e2d6909ea47fdda6e9a82
Mitchell Bregman <mitchbregs@gmail.com>
2020-02-08 14:35:16 -0500

    testing

--- notebooks/src/secrets_object.py
+++ notebooks/src/secrets_object.py
@@ -1,31 +1,86 @@
-from dotenv import load_dotenv
+from typing import Union
+
+import dotenv
 from pathlib import Path
 
 
+# TODO: make a `JiggyException` superclass and `NotFoundError` a subclass
 class JiggySecretsError(Exception):
     pass
 
 
-class Secrets:
+class JiggySecrets:
     """Class handling secrets"""
     def __init__(self, path: str):
-        self.path = _check_valid(path=Path(path))
+        self._path = self._check_valid(path=path)
 
-    def _check_valid(self, path: Path):
-        if path.exists() and path.is_file():
-            return path
-        else:
-            raise JiggySecretsError('unable to find secrets configuration path')
-    
-    def from_env():
-        load_dotenv(dotenv_path=self.path)
+    def __repr__(self):
+        return '<JiggySecrets `{path}`, `{type}`>'.format(path=self._path)
 
-    def from_yaml():
+    @property
+    def path(self):
+        return self._path
 
-    def from_json():
+    @property
+    def type(self):
+        return self._type
 
-    def from_pyfile():
+    @staticmethod
+    def _check_valid(path: str) -> Union[Path, JiggySecretsError]:
+        """Checks validity of secrets configuration path provided.
 
-    def config(self):
-        """Abstraction for a secrets configuration class"""
+        Args:
+            path (str): String representation of secrets configuration path
+
+        Raises:
+            JiggySecretsError (Exception): if the secrets filepath does not
+                exist or is not a file
+
+        Returns:
+            path (pathlib.Path): PosixPath of validated secrets configuration
+                path
+        """
+
+        path = Path(path)
+
+        if not path.exists():
+            raise JiggySecretsError('secrets configuration does not exist')
+
+        if not path.is_file():
+            raise JiggySecretsError('secrets configuration must be a file')
+
+        return path
+
+    def load(self) -> dict:
         raise NotImplementedError()
+
+
+class EnvSecrets(JiggySecrets):
+    """Sublass of ``Secrets`` handling `.env` based secrets"""
+    def __init__(self, path):
+        super(EnvSecrets, self).__init__(path)
+
+    def __repr__(self):
+        return '<EnvSecrets `{path}`>'.format(path=self.path)
+
+    def load(self) -> dict:
+        secrets = dotenv.main.dotenv_values(dotenv_path=self.path)
+        return secrets
+
+
+    # def from_env(self) -> dict:
+    #     load_dotenv(dotenv_path=self.path)
+    #     import ipdb; ipdb.set_trace()
+
+    # def from_yaml():
+    #     pass
+
+    # def from_json():
+    #     pass
+
+    # def from_pyfile():
+    #     pass
+
+    # def config(self):
+    #     """Abstraction for a secrets configuration class"""
+    #     raise NotImplementedError()
