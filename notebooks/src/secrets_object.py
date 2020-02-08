de2be968160cc3e5335f2ab75570792463efdae5
Mitchell Bregman <mitchbregs@gmail.com>
2020-02-08 15:16:36 -0500

    cleanup

--- notebooks/src/secrets_object.py
+++ notebooks/src/secrets_object.py
@@ -96,27 +96,24 @@ class YAMLSecrets(JiggySecrets):
         return secrets
 
 
-class PySecrets(JiggySecrets):
-    """Sublass of ``Secrets`` handling `Python` based secrets"""
-    def __init__(self, path: str):
-        super(PySecrets, self).__init__(path)
+# class PySecrets(JiggySecrets):
+#     """Sublass of ``Secrets`` handling `Python` based secrets"""
+#     def __init__(self, path: str):
+#         super(PySecrets, self).__init__(path)
 
-    def __repr__(self):
-        return '<PySecrets `{path}`>'.format(path=self.path)
+#     def __repr__(self):
+#         return '<PySecrets `{path}`>'.format(path=self.path)
 
-    @staticmethod
-    def _parse_module(path) -> dict:
-        """Returns a dictionary containing `package` and `module` import"""
-        import_path = str(path)
-        package = '.'.join(import_path.split('/')[:-1])
-        module = import_path.split('/')[-1].split('.')[0]
-        return {
-            'package': package,
-            'module': module
-        }
+#     @staticmethod
+#     def _parse_module(path) -> dict:
+#         """Returns a dictionary containing `package` and `module` import"""
+#         import_path = str(path)
+#         package = '.'.join(import_path.split('/')[:-1])
+#         module = import_path.split('/')[-1].split('.')[0]
+#         return package, module
 
-    def load(self) -> dict:
-        """Returns a dictionary containing all secrets"""
-        imports = self._parse_module(self.path)
-        mdl = getattr(import_module(imports['package']), imports['module'])
-        import ipdb; ipdb.set_trace()
+#     def load(self) -> dict:
+#         """Returns a dictionary containing all secrets"""
+#         package, module = self._parse_module(self.path)
+#         mdl = getattr(import_module(imports['package']), imports['module'])
+#         import ipdb; ipdb.set_trace()
