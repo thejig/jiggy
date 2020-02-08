a3fcd3063022127a1ceb826916ed99993b2bce4d
leonkozlowski <leonkozlowski@gmail.com>
2020-02-08 18:01:43 -0500

    fix filepaths

--- examples/utils/dates.py
+++ examples/utils/dates.py
@@ -4,21 +4,26 @@ from src.jiggy.task import Task
 
 class GetDateTask(Task):
     def __init__(self, name):
-        self.name = name
+        self._name = name
         super(GetDateTask, self).__init__(name)
+
     def run(self):
         return datetime.datetime.now()
 
+
 class GetWeekdayTask(Task):
     def __init__(self, name):
-        self.name = name
+        self._name = name
         super(GetWeekdayTask, self).__init__(name)
+
     def run(self, x):
-        return x.strftime('%A')
+        return x.strftime("%A")
+
 
 class GetFirstLetter(Task):
     def __init__(self, name):
-        self.name = name
+        self._name = name
         super(GetFirstLetter, self).__init__(name)
+
     def run(self, x):
         return x[0]
