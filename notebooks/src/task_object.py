2bd250565210acd8b257bab9f4ac7f5c795d6126
Mitchell Bregman <mitchbregs@gmail.com>
2020-02-07 20:18:38 -0500

    class initialization

---
+++ notebooks/src/task_object.py
@@ -0,0 +1,7 @@
+class Task(object):
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return '<JigTask `{}`>'.format(self.name)
+    def run(self):
+        raise NotImplementedError()
--- notebooks/src/utils.py
+++ notebooks/src/utils.py
@@ -1,10 +1,10 @@
-import random
+import datetime
+from src.task_object import Task
 
-def random_num():
-    return random.randint(0, 10)
 
-def add(x, y):
-    return x+y
-
-def multiply(x, y):
-    return x*y
+class GetDateTask(Task):
+    def __init__(self, name):
+        self.name = name
+        super(GetDateTask, self).__init__(name)
+    def run(self):
+        return datetime.datetime.now()
