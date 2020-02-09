1abae4ad6502e715d40102433a5d47c8dd6c326a
leonkozlowski <leonkozlowski@gmail.com>
2020-02-09 10:00:45 -0500

    fix tets + tox

--- tests/testJiggy/test_manager.py
+++ tests/testJiggy/test_manager.py
@@ -1,37 +1,35 @@
 """Test Manager Classes."""
-import pytest
-
 from unittest import TestCase
 
-from src.jiggy.manager import Manager
+from src.jiggy.pipeline import Pipeline
 
 
-class TestManager(TestCase):
+class TestPipeline(TestCase):
     """Test suite for src.jiggy.manager."""
 
     def __init__(self, path):
         """Construct TestCase."""
-        self.test_manager = Manager(
+        self.test_pipeline = Pipeline(
             location="testData/minimal_test_case.yml"
         )
-        super(TestManager, self).__init__(path)
+        super(TestPipeline, self).__init__(path)
 
     def test_construct(self):
         """Test object type."""
-        assert isinstance(self.test_manager, Manager)
+        assert isinstance(self.test_pipeline, Pipeline)
 
     def test_properties(self):
         """Test property decorators."""
-        assert self.test_manager.name == "Gets todays date, then finds weekday"
-        assert self.test_manager.author == "xyz@company.com"
-        assert self.test_manager.version == "0.0.1"
-        assert self.test_manager.description == (
+        assert self.test_pipeline.name == "Gets todays date, then finds weekday"
+        assert self.test_pipeline.author == "xyz@company.com"
+        assert self.test_pipeline.version == "0.0.1"
+        assert self.test_pipeline.description == (
             "Creates a database, gets a date, pushes to database"
         )
-        assert isinstance(self.test_manager.pipeline, dict)
-        assert self.test_manager.secrets == {
+        assert isinstance(self.test_pipeline.pipeline, dict)
+        assert self.test_pipeline.secrets == {
             "location": ".env",
             "source": "jiggy.EnvSecrets"
         }
-        assert self.test_manager.executor == "sequential"
-        assert isinstance(self.test_manager.associate, tuple)
+        assert isinstance(self.test_pipeline.tasks, list)
+        assert self.test_pipeline.executor == "sequential"
